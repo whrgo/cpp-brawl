390e62bdfbdcd898255b7e47612bb9a0ca1711ec
SB-9838 <86655602+SB-9838@users.noreply.github.com>
2022-03-27 19:08:50 +0300

    Create config.h

---
+++ config.h
@@ -0,0 +1,17 @@
+namespace CONFIG
+{
+	// shop supported cc
+	std::string SupportedContentCreator = "S.B";
+	// region/location
+	std::string Region = "LB";
+	// trophies count
+	int Trophies = 99999;
+	// Star Points count
+	int StarPoints = 99999;
+	// Gold/Coins count
+	int Gold = 99999;
+	// Gems/Diamonds count
+	int Gems = 99999;
+	// print game analytics to terminal
+	bool LogGameAnalytics = true;
+}
